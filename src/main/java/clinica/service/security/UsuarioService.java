fdfa92d2c576df6939975a041a183211c8614db2
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-04-05 09:02:32 -0300

    remove duplicate loadUserByUsername

--- src/main/java/clinica/service/security/UsuarioService.java
+++ src/main/java/clinica/service/security/UsuarioService.java
@@ -5,24 +5,15 @@ import clinica.exceptions.BadRequestException;
 import clinica.repository.security.IRolRepository;
 import clinica.repository.security.IUsuarioRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class UsuarioService implements UserDetailsService {
+public class UsuarioService {
   private final IUsuarioRepository usuarioRepository;
   private final IRolRepository rolRepository;
   private final PasswordEncoder passwordEncoder;
-
   
   public Usuario guardar(Usuario usuario) throws BadRequestException {
     if (usuario == null)
@@ -31,18 +22,5 @@ public class UsuarioService implements UserDetailsService {
     usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
     return usuarioRepository.save(usuario);
   }
-  
-  @Override
-  @Transactional(readOnly = true)
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return usuarioRepository.findByUsername(username)
-        .map(usuario -> User.builder()
-            .username(usuario.getUsername())
-            .password(usuario.getPassword())
-            .authorities(usuario.getRoles().stream()
-                .map(rol -> new SimpleGrantedAuthority(rol.getName()))
-                .collect(Collectors.toSet()))
-            .build())
-        .orElseThrow(() -> new UsernameNotFoundException("No existe el usuario con username: " + username));
-  }
+
 }
